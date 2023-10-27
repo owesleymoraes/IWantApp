ab2f3ed67934c91359146f39f0a14c573dae0399
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-10-27 08:53:03 -0300

    refactor: adding field Action in the entity

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -2,6 +2,7 @@ namespace _4_IWantApp.Domain.Products
 {
     public class Category : BaseEntity
     {
-
+        public string Name { get; set; }
+        public bool Active { get; set; } = true;
     }
 }
--- Domain/Products/Product.cs
+++ Domain/Products/Product.cs
@@ -3,10 +3,11 @@ namespace _4_IWantApp.Domain.Products
     public class Product : BaseEntity
     {
         public string Name { get; set; }
-        public bool HasStock { get; set; }
         public int CategoryId { get; set; }
         public Category Category { get; set; }
+        public bool HasStock { get; set; }
         public string Description { get; set; }
+        public bool Active { get; set; } = true;
 
     }
 }
