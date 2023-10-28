c5650edee701223df845a361d215f3909a9bf632
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-10-28 19:18:16 -0300

    feat: created connection with database

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using _4_IWantApp.Endpoints.Categories;
+using _4_IWantApp.Infra.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionString:IWantDb"]);
 
 // Add services to the container.
 // Learn more about configuration Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
