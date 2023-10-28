a92887416db84260780cd566b8aa1a9eb3e4fa59
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-10-28 20:19:56 -0300

    feat: implements dto of the category entity

---
+++ Endpoints/DTO/CategoryRequest.cs
@@ -0,0 +1,10 @@
+namespace _4_IWantApp.Endpoints.Categories
+{
+    public class CategoryRequest
+    {
+        public string Name { get; set; }
+        public bool Active { get; set; }
+
+    }
+
+}
---
+++ Endpoints/DTO/CategoryResponse.cs
@@ -0,0 +1,11 @@
+namespace _4_IWantApp.Endpoints.Categories
+{
+    public class CategoryResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+
+    }
+
+}
