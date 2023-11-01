c4b0d962f870e9b842e9b9c4468219aa82709741
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-10-31 22:25:17 -0300

    feat: application of the flunt in entity

--- Domain/Products/Category.cs
+++ Domain/Products/Category.cs
@@ -1,8 +1,28 @@
+using Flunt.Validations;
+
 namespace _4_IWantApp.Domain.Products
 {
     public class Category : BaseEntity
     {
         public string Name { get; set; }
         public bool Active { get; set; } = true;
+
+        public Category(string name, string createdBy, string editedBy)
+        {
+            var contract = new Contract<Category>()
+             .IsNotNullOrEmpty(name, "Name", "Nome é obrigatório")
+             .IsNotNullOrEmpty(createdBy, "CreatedBy", "Nome do criador da categoria é obrigatório")
+             .IsNotNullOrEmpty(editedBy, "EditedBy", "Editar é um campo obrigatório");
+            AddNotifications(contract);
+
+            Name = name;
+            Active = true;
+            EditedBy = editedBy;
+            CreatedBy = createdBy;
+            CreatedOn = DateTime.Now;
+            EditedOn = DateTime.Now;
+
+        }
+
     }
 }
