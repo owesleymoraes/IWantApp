301e51d854ba8e9e206617d03d43ecbd2cfc911b
Wesley Moraes"git config --global user.name Wesley <wesleyjava@gmail.com>
2023-11-11 18:39:53 -0300

    feat: adding resource of the observation in save and update

--- Endpoints/Categories/CategoryPost.cs
+++ Endpoints/Categories/CategoryPost.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using _4_IWantApp.Domain.Products;
 using _4_IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Authorization;
@@ -9,11 +10,12 @@ namespace _4_IWantApp.Endpoints.Categories
         public static string Template => "/categories";
         public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
         public static Delegate Handle => Action;
-        
-        [Authorize]
-        public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
+
+        [Authorize(Policy = "EmployeePolicy")]
+        public static IResult Action(CategoryRequest categoryRequest, HttpContext http, ApplicationDbContext context)
         {
-            var category = new Category(categoryRequest.Name, "Wesley", "Wesley");
+            string userId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            var category = new Category(categoryRequest.Name, userId, userId);
 
             if (!category.IsValid)
             {
